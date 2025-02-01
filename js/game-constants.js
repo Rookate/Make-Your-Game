aca639ba0d16d379ab03c9b807a78e1381eaedcc
Gabriel K <gab270b@gmail.com>
2025-02-01 22:16:03 +0100

    modify some property

--- js/game-constants.js
+++ js/game-constants.js
@@ -2,23 +2,23 @@ export const GAME_CONFIG = {
     PLAYER: {
         SPEED: 5,
         LIVES: 3,
-        FIRE_RATE: 300,
+        FIRE_RATE: 200,
     },
     PROJECTILE: {
         SPEED: 5,
     },
     ENEMIES: {
         SPEED: 5,
-        STEP_DOWN: 20,
+        STEP_DOWN: 10,
         WIDTH: 60,
         HEIGHT: 40,
         ROWS: 3,
         COLS: 7,
         START_Y: 50,
-        HEALTH: 1,
+        HEALTH: 2,
         SPECIAL_START_WAVE: 10,
         SPECIAL_HEALTH: 3,
-        FIRE_RATE: 800,
+        FIRE_RATE: 1000,
     },
     DIFFICULTY: {
         minProjectileRate: 100, // Limite minimale
