f9e2cab4eeba2afbbfa8b7bb542ffbb42d76b993
Gabriel K <gab270b@gmail.com>
2025-02-01 22:21:50 +0100

    don't need script anymore

--- script.js
+++
@@ -1,9 +0,0 @@
-// G. Sons et animations
-
-function playSound() {
-    // TODO : Jouer un effet sonore spécifique (tir, explosion, etc.).
-}
-
-function animateExplosion() {
-    // TODO : Ajouter une animation d'explosion lorsqu'un ennemi est détruit.
-}
