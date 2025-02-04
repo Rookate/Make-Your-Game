883703e50c5c630a9b4451b633e466de0af07a76
banbi <banbi@example.com>
2025-02-04 13:46:41 +0100

    balle probleme

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  new Game();
-});
+import { Player } from "./player";
+
+class Game {
+  constructor() {
+    this.container = document.querySelector(".game-container");
+    this.player = new Player(this.container); // Création du joueur
+
+    this.startGame();
+  }
+
+  startGame() {
+    console.log("Le jeu commence !");
+  }
+}
+const game = new Game();
