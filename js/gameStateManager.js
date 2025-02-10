89d330b2b66825a61fd32eaa6fa9e015779477c9
pcaboor <pierre.caboor59@gmail.com>
2025-02-10 13:22:45 +0100

    class and clean code for sequence

--- js/gameStateManager.js
+++ js/gameStateManager.js
@@ -5,6 +5,7 @@ import { Enemy, KamikazeEnemy } from './ennemyClass.js';
 import { GAME_CONFIG } from './game-constants.js';
 import { createLeaderBoard } from './leaderBoard.js';
 import { Player } from './player.js';
+import { SequenceCinematic } from './sequenceCinematic.js';
 import { displayWaveMessage, updateLivesDisplay, updateScoreDisplay, updateWaveDisplay } from './ui.js';
 import { throttle } from './utils.js';
 
@@ -40,6 +41,7 @@ export class GameStateManager {
 
 
         // this.transition = new SceneTransition();
+        this.sequence = new SequenceCinematic();
         this.cinematic = new CinematicManager();
         this.isGameStarted = false;
         this.waveStarted = false;
@@ -107,59 +109,8 @@ export class GameStateManager {
         if (this.isGameStarted) return;
         this.hideAllScreens();
 
-        const cinematic = this.cinematic;
-
-        cinematic.createEvent('titleText', CinematicEventType.SCENE, {
-            duration: 3000,
-            text: 'Welcome to the game',
-            className: 'cut-scene',
-            planet: "css/Lava.png"
-        });
-        cinematic.createEvent('titleText3', CinematicEventType.SCENE, {
-            duration: 3000,
-            text: 'The invader come and destroy our planet',
-            className: 'cut-scene',
-            planet: "css/Terran.png"
-        });
-        cinematic.createEvent('titleText4', CinematicEventType.SCENE, {
-            duration: 4000,
-            text: 'You are the last chance',
-            className: 'cut-scene',
-            planet: "css/Ice.png"
-        });
-        cinematic.createEvent('playerAppear', CinematicEventType.PLAYER, {
-            duration: 5000,
-            className: 'cutscene',
-            text: 'Space June 2079 9pm',
-            playerImage: 'css/pngkey.com-galaga-ship-png-1472422.png',
-            backgroundImage: "css/HD-wallpaper-artistic-pixel-art-space-moon.jpg"
-        });
-        cinematic.createEvent('zoomPlayer', CinematicEventType.PLAYER, {
-            duration: 4000,
-            className: 'cutscene',
-            text: 'Leader to falcon I approach the targets',
-            backgroundImage: 'css/471a0d1d9af520c394e02a09120fc7c1.gif'
-        });
-        cinematic.createEvent('zoomPlayer2', CinematicEventType.PLAYER, {
-            duration: 5000,
-            className: 'cutscene',
-            text: 'Ok leader you can engage',
-            backgroundImage: 'css/f2162a69274b7cb1f9330c4580f28723.gif'
-        });
-        cinematic.createEvent('zoomPlayer3', CinematicEventType.PLAYER, {
-            duration: 2000,
-            className: 'cutscene',
-            text: 'Prepare to fight !',
-            backgroundImage: 'css/471a0d1d9af520c394e02a09120fc7c1.gif'
-        });
-        //,
-        try {
-            //   await transition.play('levelStart');
-            await cinematic.playSequence(['titleText', 'titleText2', 'titleText3', 'titleText4', 'playerAppear', 'zoomPlayer', 'zoomPlayer2', 'zoomPlayer3']);
+        if (await this.sequence.playSequence()) {
             this.launchGame();
-        } catch (error) {
-            console.error('Failed to start game:', error);
-            this.cleanup();
         }
     }
 
@@ -205,29 +156,12 @@ export class GameStateManager {
 
     async launchGameOver() {
         this.hideAllScreens();
-
-        const cinematic = this.cinematic;
-
-        cinematic.createEvent('titleText', CinematicEventType.PLAYER, {
-            duration: 3000,
-            text: 'Game Over, Invaders win',
-            className: 'cut-scene',
-            playerImage: "css/galaga-ship.png",
-            backgroundImage: "css/83024e2e5cd4fc4261e4b97dc93ed12c.gif"
-        });
-
-        try {
-            //   await transition.play('levelStart');
-            await cinematic.playSequence(['titleText']);
+        if (await this.sequence.playGameOverSequence()) {
             this.gameOver();
-        } catch (error) {
-            console.error('Failed to start game:', error);
-            this.cleanup();
         }
     }
 
     async gameOver() {
-
         this.state.currentState = 'GAME_OVER';
         this.ui.endMessage.classList.remove('hidden');
         this.ui.gameContainer.classList.add('hidden')
@@ -585,7 +519,7 @@ export class GameStateManager {
 
 
     // Wave
-    startNextWave() {
+    async startNextWave() {
         if (this.state.waveTransition) return;
         this.state.waveTransition = true;
         this.state.wave += 1;
@@ -597,40 +531,9 @@ export class GameStateManager {
             GAME_CONFIG.ENEMIES.HEALTH = GAME_CONFIG.ENEMIES.SPECIAL_HEALTH;
         }
 
-        const cinematic = this.cinematic;
-        cinematic.createEvent(`waveIntro${this.state.wave}`, CinematicEventType.PLAYER, {
-            duration: 2000,
-            text: `Wave ${this.state.wave} Incoming!`,
-            className: 'cut-scene',
-            backgroundImage: 'css/885542.png'
-        });
-
-        cinematic.createEvent(`waveBriefing${this.state.wave}`, CinematicEventType.PLAYER, {
-            duration: 3000,
-            text: `Prepare for stronger enemies!`,
-            className: 'cut-scene',
-            backgroundImage: 'css/885542.png'
-        });
-
-        if (this.state.wave % 2 === 0) {
-            cinematic.createEvent(`waveSpecial${this.state.wave}`, CinematicEventType.PLAYER, {
-                duration: 3000,
-                text: `Special enemies appear every 5 waves!`,
-                className: 'cut-scene-special',
-                backgroundImage: 'css/HD-wallpaper-artistic-pixel-art-space-moon.jpg'
-            });
+        if (await this.sequence.playWaveSquence()) {
+            this.generateEnemies();
         }
-
-        cinematic.playSequence([`waveIntro${this.state.wave}`, `waveSpecial${this.state.wave}`, `waveBriefing${this.state.wave}`])
-            .then(() => {
-                this.state.waveTransition = false;
-                this.generateEnemies();
-            })
-            .catch(error => {
-                console.error('Wave cinematic failed:', error);
-                this.state.waveTransition = false;
-                this.generateEnemies();
-            });
     }
 
     async leaderBoard() {
---
+++ js/sequenceCinematic.js
@@ -0,0 +1,122 @@
+import { CinematicEventType, CinematicManager } from './cinematicManager.js';
+
+export class SequenceCinematic {
+    constructor(id, events) {
+        this.id = id;
+        this.events = events;
+        this.cinematic = new CinematicManager();
+    }
+
+    async playSequence() {
+        const cinematic = this.cinematic;
+
+        // cinematic.createEvent('titleText', CinematicEventType.SCENE, {
+        //     duration: 3000,
+        //     text: 'Welcome to the game',
+        //     className: 'cut-scene',
+        //     planet: "css/Lava.png"
+        // });
+        // cinematic.createEvent('titleText3', CinematicEventType.SCENE, {
+        //     duration: 3000,
+        //     text: 'The invader come and destroy our planet',
+        //     className: 'cut-scene',
+        //     planet: "css/Terran.png"
+        // });
+        // cinematic.createEvent('titleText4', CinematicEventType.SCENE, {
+        //     duration: 4000,
+        //     text: 'You are the last chance',
+        //     className: 'cut-scene',
+        //     planet: "css/Ice.png"
+        // });
+        // cinematic.createEvent('playerAppear', CinematicEventType.PLAYER, {
+        //     duration: 5000,
+        //     className: 'cutscene',
+        //     text: 'Space June 2079 9pm',
+        //     playerImage: 'css/pngkey.com-galaga-ship-png-1472422.png',
+        //     backgroundImage: "css/HD-wallpaper-artistic-pixel-art-space-moon.jpg"
+        // });
+        // cinematic.createEvent('zoomPlayer', CinematicEventType.PLAYER, {
+        //     duration: 4000,
+        //     className: 'cutscene',
+        //     text: 'Leader to falcon I approach the targets',
+        //     backgroundImage: 'css/471a0d1d9af520c394e02a09120fc7c1.gif'
+        // });
+        // cinematic.createEvent('zoomPlayer2', CinematicEventType.PLAYER, {
+        //     duration: 5000,
+        //     className: 'cutscene',
+        //     text: 'Ok leader you can engage',
+        //     backgroundImage: 'css/f2162a69274b7cb1f9330c4580f28723.gif'
+        // });
+        // cinematic.createEvent('zoomPlayer3', CinematicEventType.PLAYER, {
+        //     duration: 2000,
+        //     className: 'cutscene',
+        //     text: 'Prepare to fight !',
+        //     backgroundImage: 'css/471a0d1d9af520c394e02a09120fc7c1.gif'
+        // });
+        //,
+
+        try {
+            //   await transition.play('levelStart');
+            await cinematic.playSequence(['titleText', 'titleText2', 'titleText3', 'titleText4', 'playerAppear', 'zoomPlayer', 'zoomPlayer2', 'zoomPlayer3']);
+            return true;
+        } catch (error) {
+            console.error('Failed to start game:', error);
+            return false;
+        }
+    }
+    async playGameOverSequence() {
+        const cinematic = this.cinematic;
+
+        cinematic.createEvent('titleText', CinematicEventType.PLAYER, {
+            duration: 3000,
+            text: 'Game Over, Invaders win',
+            className: 'cut-scene',
+            playerImage: "css/galaga-ship.png",
+            backgroundImage: "css/83024e2e5cd4fc4261e4b97dc93ed12c.gif"
+        });
+        try {
+            await cinematic.playSequence(['titleText']);
+            return true
+        } catch (error) {
+            console.error('Failed to start game:', error);
+            return false
+        }
+    }
+
+    async playWaveSquence() {
+        const cinematic = this.cinematic;
+        cinematic.createEvent(`waveIntro${window.gameState.wave}`, CinematicEventType.PLAYER, {
+            duration: 2000,
+            text: `Wave ${window.gameState.wave} Incoming!`,
+            className: 'cut-scene',
+            backgroundImage: 'css/885542.png'
+        });
+
+        cinematic.createEvent(`waveBriefing${window.gameState.wave}`, CinematicEventType.PLAYER, {
+            duration: 3000,
+            text: `Prepare for stronger enemies!`,
+            className: 'cut-scene',
+            backgroundImage: 'css/885542.png'
+        });
+
+        if (window.gameState.wave % 2 === 0) {
+            cinematic.createEvent(`waveSpecial${window.gameState.wave}`, CinematicEventType.PLAYER, {
+                duration: 3000,
+                text: `Special enemies appear every 5 waves!`,
+                className: 'cut-scene-special',
+                backgroundImage: 'css/HD-wallpaper-artistic-pixel-art-space-moon.jpg'
+            });
+        }
+
+        cinematic.playSequence([`waveIntro${window.gameState.wave}`, `waveSpecial${window.gameState.wave}`, `waveBriefing${window.gameState.wave}`])
+            .then(() => {
+                window.gameState.waveTransition = false;
+                return true;
+            })
+            .catch(error => {
+                console.error('Wave cinematic failed:', error);
+                window.gameState.waveTransition = false;
+                return false;
+            });
+    }
+}
