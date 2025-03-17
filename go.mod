939f981ccf8df8424592cf1ddec0e88d81e5fabe
Gabriel K <gab270b@gmail.com>
2025-03-17 15:39:19 +0100

    production

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module spaceInvader
 
 go 1.23.1
-
-require github.com/mattn/go-sqlite3 v1.14.24
--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	"spaceInvader/server"
 )
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./css"))))
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.HandleFunc("/api/postScore", server.ScoreHandler)
-	http.HandleFunc("/api/getScore", server.GetScoreHandler)
 
 	log.Println("Server started on http://localhost:8081")
 	err := http.ListenAndServe(":8081", nil)
